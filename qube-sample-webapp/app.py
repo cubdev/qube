daca738103983cb8b82b67204dbc07da17244430
cartmancodes <shubhchak@gmail.com>
2021-04-03 14:06:33 +0530

    Containerized sample flask web-app

---
+++ qube-sample-webapp/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+
+#Set Flask context here
+app = Flask(__name__)
+
+CAREERS = ["SMOKE WEED"]
+
+@app.route("/")
+def home():
+	return render_template("index.html")
+
+@app.route("/careers", methods = ['GET', 'POST'])
+def get_career():
+	global CAREERS
+	if request.method == "GET":
+		return ','.join(CAREERS)
+	elif request.method == "POST":
+		CAREERS.append(request.data)
+	
+
+if __name__ == "__main__":
+	app.run(debug=True, host = "0.0.0.0")
