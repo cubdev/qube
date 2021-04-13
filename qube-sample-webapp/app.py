2b5242ff6293f33fb1f020f7b727f5f9e5c69b8c
cartmancodes <shubhchak@gmail.com>
2021-04-13 12:55:40 +0530

    Incorporated mongodb as a separate container via compose

--- qube-sample-webapp/app.py
+++ qube-sample-webapp/app.py
@@ -1,22 +1,28 @@
 from flask import Flask, request, render_template
+from dbi import get_questions, post_questions
 
 #Set Flask context here
 app = Flask(__name__)
-
-CAREERS = ["SMOKE WEED"]
 
 @app.route("/")
 def home():
 	return render_template("index.html")
 
-@app.route("/careers", methods = ['GET', 'POST'])
-def get_career():
-	global CAREERS
-	if request.method == "GET":
-		return ','.join(CAREERS)
-	elif request.method == "POST":
-		CAREERS.append(request.data)
-	
+@app.route("/getQuestions", methods = ['GET'])
+def get():
+	questions = get_questions()
+	return questions
+
+@app.route("/addQuestions", methods = ['POST'])
+def post():
+	response = ""
+	try:
+		post_questions(request.json)
+		response = "DONE!"
+	except Exception as e:
+		response = "FAILED TO UPDATE, check input data"
+	return response
+
 
 if __name__ == "__main__":
 	app.run(debug=False, host="0.0.0.0", port=5000)
